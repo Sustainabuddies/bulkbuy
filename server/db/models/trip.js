c0a2067ddb63770022213c2fd3ddd901a56fa4f6
JWaltuch <jenna.waltuch@gmail.com>
2020-04-24 21:22:46 -0400

    fix: revised naming

--- server/db/models/trip.js
+++ server/db/models/trip.js
@@ -2,11 +2,11 @@ const Sequelize = require('sequelize')
 const db = require('../db')
 
 const Trip = db.define('trip', {
-  dropOffLatitude: {
+  exchangePointLatitude: {
     type: Sequelize.INTEGER,
     allowNull: false
   },
-  dropOffLongitude: {
+  exchangePointLongitude: {
     type: Sequelize.INTEGER,
     allowNull: false
   },

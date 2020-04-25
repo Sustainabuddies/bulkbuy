3d5085e353352e4b3c419ca471f0617fa0ca64eb
Olivia <oliviasztanga@gmail.com>
2020-04-25 14:01:46 -0400

    fix: taking different api approach

--- client/components/select-store.js
+++ client/components/select-store.js
@@ -1,27 +1,72 @@
-import React, {Component} from 'react'
+import React, {useState, useEffect} from 'react'
+import axios from 'axios'
 
-import {Map, GoogleApiWrapper} from 'google-maps-react'
+import {Map, GoogleApiWrapper, Marker} from 'google-maps-react'
 import {googleApiKey} from '../../secrets'
 
-class SelectStore extends Component {
-  constructor() {
-    super()
-    this.state = {currentLocation: [0, 0]}
-  }
+const SelectStore = props => {
+  const [currentLocation, setCurrentLocation] = useState({
+    latitude: 0,
+    longitude: 0
+  })
+  const [searchQuery, setSearchQuery] = useState('')
 
-  componentDidMount() {
+  useEffect(() => {
     if (navigator && navigator.geolocation) {
       navigator.geolocation.getCurrentPosition(position => {
         const {coords} = position
-        this.setState({currentLocation: [coords.latitude, coords.longitude]})
+        setCurrentLocation({
+          latitude: coords.latitude,
+          longitude: coords.longitude
+        })
       })
     }
+  }, [])
+
+  const handleSubmit = async evt => {
+    evt.preventDefault()
+    const map = new google.maps.Map()
+    const service = new google.maps.places.PlacesService(map)
+    service.textSearch({query: searchQuery}, (results, status) =>
+      console.log(results)
+    )
   }
 
-  render() {
-    console.log(this.state)
-    return <Map google={this.props.google} zoom={14} />
-  }
+  return (
+    <div className="select-store">
+      <form className="select-store__form" onSubmit={handleSubmit}>
+        <label className="select-store__label">
+          Search for a store:
+          <input
+            className="select-store__input"
+            type="text"
+            value={searchQuery}
+            onChange={e => setSearchQuery(e.target.value)}
+          />
+        </label>
+        <button className="select-store__btn" type="submit">
+          Search
+        </button>
+      </form>
+
+      <div id="map" />
+
+      <div className="select-store__map">
+        {currentLocation.latitude ? (
+          <Map
+            google={props.google}
+            zoom={12}
+            initialCenter={{
+              lat: currentLocation.latitude,
+              lng: currentLocation.longitude
+            }}
+          />
+        ) : (
+          'Please share your location to use this app!'
+        )}
+      </div>
+    </div>
+  )
 }
 
 export default GoogleApiWrapper({apiKey: googleApiKey})(SelectStore)

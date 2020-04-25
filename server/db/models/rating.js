b092cef90f1f908aaf6cd557bba948a7968aa91a
Athena Chen <athenachenn@gmail.com>
2020-04-25 14:16:47 -0400

    Update rating.js

--- server/db/models/rating.js
+++ server/db/models/rating.js
@@ -2,14 +2,6 @@ const Sequelize = require('sequelize')
 const db = require('../db')
 
 const Rating = db.define('rating', {
-  raterId: {
-    type: Sequelize.INTEGER,
-    allowNull: false
-  },
-  personRatingId: {
-    type: Sequelize.INTEGER,
-    allowNull: false
-  },
   rating: {
     type: Sequelize.INTEGER,
     allowNull: false

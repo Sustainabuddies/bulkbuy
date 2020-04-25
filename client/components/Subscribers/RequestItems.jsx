61f4e2a7c5795fff599b038b4282a30c261179a0
Athena Chen <athenachenn@gmail.com>
2020-04-25 14:55:55 -0400

    fleshed out structure of request-items page

--- client/components/Subscribers/RequestItems.jsx
+++ client/components/Subscribers/RequestItems.jsx
@@ -1,6 +1,5 @@
 import React, {Fragment, useState} from 'react'
 import './request-items.css'
-import classnames from 'classnames'
 
 export const RequestItems = () => {
   const [searchValue, setSearchValue] = useState('')
@@ -14,6 +13,17 @@ export const RequestItems = () => {
       // TODO: add ajax call here when the user searches for an item
     }
   }
+
+  const handleAddItem = e => {
+    e.preventDefault()
+    // add to the subscriber's cart
+  }
+
+  const neighbors = [
+    {name: 'Angelina', rating: 4},
+    {name: 'Timothy', rating: 3.5},
+    {name: 'Ashley', rating: 4.5}
+  ]
 
   return (
     <Fragment>
@@ -31,7 +41,27 @@ export const RequestItems = () => {
               placeholder="Search for an item"
               onKeyDown={e => handleEnter(e)}
             />
-          </div>
+          </div>{' '}
+          <p />
+          <h2>Neighbors in your area</h2>
+          {neighbors.map((neighbor, idx) => (
+            <div className="buyer-card" key={idx}>
+              <div className="buyer-card-left">
+                {/* TODO: Pass in props here */}
+                <div style={{letterSpacing: '.15em'}}>
+                  {neighbor.name.toUpperCase()}
+                </div>
+                <div style={{fontSize: '12px'}}>Rating: {neighbor.rating}</div>
+              </div>
+              <button
+                type="button"
+                className="buyer-card-button"
+                onClick={e => handleAddItem(e)}
+              >
+                +
+              </button>
+            </div>
+          ))}
         </div>
 
         <div className="container" />

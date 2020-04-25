c575359abb2ed8f083eee7355d3cc72167a9c561
JWaltuch <jenna.waltuch@gmail.com>
2020-04-24 20:48:53 -0400

    Feat: added buttons to match wireframe

--- client/components/user-home.js
+++ client/components/user-home.js
@@ -7,10 +7,16 @@ import {connect} from 'react-redux'
  */
 export const UserHome = props => {
   const {username} = props
+  console.log(props)
 
   return (
     <div>
-      <h3>Welcome, {username}</h3>
+      <h3>Welcome Back, {username}</h3>
+      <div>
+        <button>I need items</button>
+        <button>I want to buy for others</button>
+        <button>Review my trips</button>
+      </div>
     </div>
   )
 }
